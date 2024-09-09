421d51e7b64382e2f6528c5ae314619ea37d320a
lcy101u <phanton0117@gmail.com>
2024-09-09 16:19:19 +0800

    add: copy function.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const exhbs = require('express-handlebars')
 const generatePassword = require('./generate_password')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.engine('handlebars', exhbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
